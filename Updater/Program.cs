5e54ec06c42c90b2c7f570d0ca605fde3ab7ea1c
Casey McQuillan <Casey@quilltechnologies.com>
2020-07-28 23:21:47 -0700

    Merging readme updater.

---
+++ Updater/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using Tweetinvi;
+
+namespace Updater
+{
+    class Program
+    {
+        public const string TWEET_ANCHOR = "<!-- BEGIN TWEETS -->";
+        public const string END_TWEET_ANCHOR = "<!-- END TWEETS -->";
+
+        static int Main(string[] args)
+        {
+            var consumerKey = args[0];
+            var consumerSecret = args[1];
+            var accessToken = args[2];
+            var accessTokenSecret = args[3];
+
+            var credentials = Auth.SetUserCredentials(consumerKey, consumerSecret, accessToken, accessTokenSecret);
+
+            var user = User.GetAuthenticatedUser(credentials);
+            var readmeText = File.ReadAllText("Readme.md");
+
+            var parameter = Timeline.CreateUserTimelineParameter();
+            parameter.IncludeRTS = false;
+            parameter.ExcludeReplies = true;
+            parameter.MaximumNumberOfTweetsToRetrieve = 7;
+
+            var timeline = Timeline.GetUserTimeline(user.Id, parameter);
+
+            var strTweets = new StringBuilder();
+
+            var tweetsStart = readmeText.IndexOf(TWEET_ANCHOR) + TWEET_ANCHOR.Length;
+            var tweetsEnd = readmeText.IndexOf(END_TWEET_ANCHOR);
+
+            strTweets.Append(readmeText[0..tweetsStart]);
+
+            foreach(var item in timeline)
+            {
+                var tweetText = item.Text.Replace("\n", "");
+                tweetText = Regex.Replace(tweetText, @"https://t\.co/[a-zA-Z0-9]+", "");
+
+                strTweets.AppendLine()
+                    .AppendFormat("#### [{0}]({1})", tweetText, item.Url)
+                    .AppendLine();
+            }
+
+            strTweets.Append(readmeText[tweetsEnd..]);
+
+            Console.WriteLine(strTweets.ToString());
+            return 0;
+        }
+    }
+}
